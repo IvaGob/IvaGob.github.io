b88dd7f7af8ace0e9f90cd1c4a164b49b16eda78
DIA <DIA@d.c>
2025-03-18 23:37:03 +0200

    fix

--- lab2/main_script.js
+++ lab2/main_script.js
@@ -19,7 +19,7 @@ function loadXML() {
                 // Створюємо HTML-картку
                 const tourDiv = document.createElement("div");
                 tourDiv.classList.add("tourDiv");
-                tourDiv.setAttribute("id", "tour-1");
+                tourDiv.setAttribute("id", `tour-${i}`);
                 tourDiv.innerHTML = `
                             <div class="tourName">
                                 <h1>${title}</h1>
